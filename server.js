98c0ef1ca887f454ff34df8d56bd3e07d59870be
catcarbonell <hello@catcarbn.com>
2020-04-10 15:12:26 -0700

    Created config file, set up and connected MongoDB Atlas DB

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
+// Connect DB
+connectDB();
+
 app.get('/', (req, res) => res.send('API is Running'));
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT), () => console.log(`Server is running on port ${PORT}`);
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 
