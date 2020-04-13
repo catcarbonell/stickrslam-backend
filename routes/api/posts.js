580c7987d5d9c75dfcf7bf07ceb8d0ab3e4b9dab
catcarbonell <hello@catcarbn.com>
2020-04-13 14:53:37 -0700

    Implemented delete post comment

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -191,4 +191,39 @@ router.post('/comment/:id',
         } 
     });
 
+// ===== DELETE COMMENT FROM POST ===== //
+
+// @route   POST  api/posts/comment/:id
+// @desc    Delete comment on a post
+// @access  Private
+router.delete('/comment/:id/:comment_id', auth, async (req,res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        // Get comment from post
+        const comment = post.comments.find(comment => comment.id === req.params.comment_id);
+
+        // Confirm comment exists
+        if(!comment){
+            return res.status(404).json({ msg: "Comment does not exist" });
+        }
+
+        // Check if user created comment
+        if(comment.user.toString() !== req.user.id){
+            return res.status(401).json({ msg: "User not authorized" })
+        }
+
+        const removeIndex=post.comments
+            .map(comment => comment.user.toString())
+            .indexOf(req.user.id);
+        post.comments.splice(removeIndex, 1);
+        await post.save();
+        
+        res.json(post.comments);
+
+    } catch(err) {
+        console.error(err.message);
+        res.status(500).send('Server Error')
+    }
+});
 module.exports = router;
